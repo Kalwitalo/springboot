a8a9b287e74e0fa5a39fb9217efb947529b53af5
kalwitaloibiapina <kalwitaloibiapina@gmail.com>
2018-08-23 10:12:49 -0300

    Atualização de classes

--- src/main/java/br/com/techlead/springboot/repository/PerfilRepository.java
+++ src/main/java/br/com/techlead/springboot/repository/PerfilRepository.java
@@ -2,9 +2,14 @@ package br.com.techlead.springboot.repository;
 
 import br.com.techlead.springboot.model.Perfil;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import javax.websocket.server.PathParam;
+import java.util.Collection;
 
 @Repository
 public interface PerfilRepository extends JpaRepository<Perfil, Integer> {
-
+    @Query("select p from Perfil p where p.nome = :nome")
+    Collection<Perfil> buscarNome(@PathParam("nome") String nome);
 }
