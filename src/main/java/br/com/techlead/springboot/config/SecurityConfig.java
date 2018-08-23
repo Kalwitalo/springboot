4e983c156836e627a0ea0658dcd604a2aea56f1a
kalwitalo <kalwitaloibiapina@gmail.com>
2018-08-23 14:37:25 -0300

    Adicionado spring devtools e adicionado spring security

---
+++ src/main/java/br/com/techlead/springboot/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package br.com.techlead.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.
+                inMemoryAuthentication()
+                .withUser("user").password("{noop}user").roles("USER").and()
+                .withUser("admin").password("{noop}admin").roles("USER","ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/perfils/**").hasRole("USER")
+                .antMatchers("/actuator/**").hasRole("ADMIN")
+                .anyRequest().denyAll()
+            .and().httpBasic()
+            .and().csrf().disable();
+    }
+}
